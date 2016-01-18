aed5c300df0af4737afed74baf2ad859670d33f0
Martin Rist <mlr-github@martinrist.com>
2016-01-18 17:54:50 +0000

    Add 'Etl' example from exercism.io

---
+++ src/main/java/com/martinrist/katas/etl/Etl.java
@@ -0,0 +1,17 @@
+package com.martinrist.katas.etl;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Implementation class for 'Etl' kata from exercism.io.
+ *
+ * @see <a href="http://exercism.io/exercises/etl/hello-world/readme"/>
+ */
+public class Etl {
+    public Map<String, Integer> transform(Map<Integer, List<String>> old) {
+
+        return null;
+
+    }
+}
