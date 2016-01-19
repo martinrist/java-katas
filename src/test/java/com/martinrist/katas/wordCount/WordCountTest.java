55c90a747f4eac87dabf9639c2ca903f2094289e
Martin Rist <mlr-github@martinrist.com>
2016-01-19 10:40:39 +0000

    Rewrite test cases to use Guava's ImmutableMap.  Add further test cases for edge cases around null / empty phrases.

--- src/test/java/com/martinrist/katas/wordCount/WordCountTest.java
+++ src/test/java/com/martinrist/katas/wordCount/WordCountTest.java
@@ -1,11 +1,13 @@
 package com.martinrist.katas.wordCount;
 
+import com.google.common.collect.ImmutableMap;
 import org.junit.Test;
 
-import java.util.HashMap;
+import java.util.Collections;
 import java.util.Map;
 
-import static org.junit.Assert.assertEquals;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
 
 /**
  * Test class for "Word Count" kata from exercism.io.
@@ -16,71 +18,72 @@ public class WordCountTest {
 
     private final WordCount wordCount = new WordCount();
 
+    @Test(expected = IllegalArgumentException.class)
+    public void nullPhrase() {
+        wordCount.phrase(null);
+    }
+
+    @Test
+    public void emptyPhrase() {
+        assertThat(wordCount.phrase(""), equalTo(Collections.EMPTY_MAP));
+    }
+
+    @Test
+    public void phraseWithOnlySpaces() {
+        assertThat(wordCount.phrase("    "), equalTo(Collections.EMPTY_MAP));
+    }
+
+    @Test
+    public void phraseWithOnlyPunctuation() {
+        assertThat(wordCount.phrase("!@£$%^"), equalTo(Collections.EMPTY_MAP));
+    }
+
     @Test
     public void countOneWord() {
-        final Map<String, Integer> expectedWordCount = new HashMap<>();
-        expectedWordCount.put("word", 1);
-
+        final Map<String, Integer> expectedWordCount = ImmutableMap.of("word", 1);
         final Map<String, Integer> actualWordCount = wordCount.phrase("word");
-        assertEquals(expectedWordCount, actualWordCount);
+        assertThat(actualWordCount, equalTo(expectedWordCount));
     }
 
     @Test
     public void countOneOfEach() {
-        final Map<String, Integer> expectedWordCount = new HashMap<>();
-        expectedWordCount.put("one", 1);
-        expectedWordCount.put("of", 1);
-        expectedWordCount.put("each", 1);
-
+        final Map<String, Integer> expectedWordCount =
+                ImmutableMap.of("one", 1, "of", 1, "each", 1);
         final Map<String, Integer> actualWordCount = wordCount.phrase("one of each");
-        assertEquals(expectedWordCount, actualWordCount);
+        assertThat(expectedWordCount, equalTo(actualWordCount));
     }
 
     @Test
     public void countMultipleOccurences() {
-        final Map<String, Integer> expectedWordCount = new HashMap<>();
-        expectedWordCount.put("one", 1);
-        expectedWordCount.put("fish", 4);
-        expectedWordCount.put("two", 1);
-        expectedWordCount.put("red", 1);
-        expectedWordCount.put("blue", 1);
+        final Map<String, Integer> expectedWordCount =
+                ImmutableMap.of("one", 1, "fish", 4, "two", 1, "red", 1, "blue", 1);
 
         final Map<String, Integer> actualWordCount = wordCount.phrase("one fish two fish red fish blue fish");
-        assertEquals(expectedWordCount, actualWordCount);
+        assertThat(expectedWordCount, equalTo(actualWordCount));
     }
 
     @Test
     public void ignorePunctuation() {
-        final Map<String, Integer> expectedWordCount = new HashMap<>();
-        expectedWordCount.put("car", 1);
-        expectedWordCount.put("carpet", 1);
-        expectedWordCount.put("as", 1);
-        expectedWordCount.put("java", 1);
-        expectedWordCount.put("javascript", 1);
-
+        final Map<String, Integer> expectedWordCount =
+                ImmutableMap.of("car", 1, "carpet", 1, "as", 1, "java", 1, "javascript", 1);
         final Map<String, Integer> actualWordCount = wordCount.phrase("car : carpet as java : javascript!!&@$%^&");
-        assertEquals(expectedWordCount, actualWordCount);
+        assertThat(expectedWordCount, equalTo(actualWordCount));
 
     }
 
     @Test
     public void includeNumbers() {
-        final Map<String, Integer> expectedWordCount = new HashMap<>();
-        expectedWordCount.put("testing", 2);
-        expectedWordCount.put("1", 1);
-        expectedWordCount.put("2", 1);
-
+        final Map<String, Integer> expectedWordCount =
+                ImmutableMap.of("testing", 2, "1", 1, "2", 1);
         final Map<String, Integer> actualWordCount = wordCount.phrase("testing, 1, 2 testing");
-        assertEquals(expectedWordCount, actualWordCount);
+        assertThat(expectedWordCount, equalTo(actualWordCount));
     }
 
     @Test
     public void normalizeCase() {
-        final Map<String, Integer> expectedWordCount = new HashMap<>();
-        expectedWordCount.put("go", 3);
-
+        final Map<String, Integer> expectedWordCount = ImmutableMap.of("go", 3);
         final Map<String, Integer> actualWordCount = wordCount.phrase("go Go GO");
-        assertEquals(expectedWordCount, actualWordCount);
+        assertThat(expectedWordCount, equalTo(actualWordCount));
     }
 
 }
