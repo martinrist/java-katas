531f4c376e6276e29b1b7feafe7d98608863a4d9
Martin Rist <mlr-github@martinrist.com>
2016-01-27 13:29:58 +0000

    Add test cases and skeleton implementation class for exercism.io 'Hamming Distance' exercise.

---
+++ src/main/java/com/martinrist/katas/hamming/Hamming.java
@@ -0,0 +1,19 @@
+package com.martinrist.katas.hamming;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Implementation class for 'Hamming Distance' kata from exercism.io.
+ *
+ * @see <a href="http://exercism.io/exercises/hamming/readme"/>
+ */
+public class Hamming {
+
+    public static int compute(String strand1, String strand2) {
+
+        // TODO: Complete implementation
+        return -1;
+
+    }
+}
